84203038463f61a7d51bd51de371e0e67d2f799b
Noah Isaacson <noah@nisaacson.com>
2013-02-01 11:27:44 -0500

    added truthy sync function

--- index.js
+++ index.js
@@ -75,7 +75,41 @@ function truthy(data, fields, cb) {
 }
 
 
+/**
+ * @param {Object} data,
+ * @param {Array} fields
+ * @return null if all required keys exist and map to truthy values
+ * @return {Array} An array containing errors about all keys that failed
+ */
+function truthySync(data, fields) {
+  var keys = Object.keys(data);
+  var errors = [];
+  var err;
+  for (var i in fields) {
+    var key = fields[i];
+    if (!data.hasOwnProperty(key)) {
+      err = {
+        message: 'key not set in data',
+        key: key,
+        value: undefined
+      };
+      errors.push(err);
+    }
+    else if (!data.key) {
+      err = {
+        message: 'value is falsy',
+        key: key,
+        value: data[key]
+      };
+      errors.push(err);
+    }
+  }
+  if (errors.length === 0) {
+    return null;
+  }
+  return errors;
+}
 exports.nonNull = nonNull;
 exports.keysOnly = keysOnly;
-
 exports.truthy = truthy;
+exports.truthySync = truthySync;
